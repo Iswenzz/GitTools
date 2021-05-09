5a81a78ced8b06521a04c4f5d216566d2ee884d0
Iswenzz <suxlolz1528@gmail.com>
2021-05-09 18:01:08 +0200

    Git: Repository class and allow empty commits at a specific date

---
+++ GitTools/Git.cs
@@ -0,0 +1,33 @@
+﻿using LibGit2Sharp;
+using System;
+
+namespace Iswenzz.GitTools
+{
+    public class Git
+    {
+        public Repository Repository { get; set; }
+
+        public Git(string repoPath)
+        {
+            Repository = new Repository(repoPath);
+        }
+
+        public void Commit(string message, DateTime date = default)
+        {
+            if (date == DateTime.MinValue)
+                date = DateTime.Now;
+
+            Signature author = new Signature(
+                Program.Settings.User.Username, 
+                Program.Settings.User.EMail,
+                date);
+            Signature committer = author;
+
+            CommitOptions options = new CommitOptions
+            {
+                AllowEmptyCommit = true
+            };
+            Repository.Commit(message, author, committer, options);
+        }
+    }
+}
