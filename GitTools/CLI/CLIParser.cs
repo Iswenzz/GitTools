c12c7dc7069f46eb83b2f41a4221d86fc71e85e0
Iswenzz <suxlolz1528@gmail.com>
2021-05-29 17:24:46 +0200

    CLIParser: Parse arguments and split the tool functions using cli verbs

---
+++ GitTools/CLI/CLIParser.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+
+namespace Iswenzz.GitTools.CLI
+{
+    public static class CLIParser
+    {
+        public static Options Options { get; set; }
+        public static CopyCommitsOptions CopyCommitsOptions { get; set; }
+
+        public static void Parse(string[] args)
+        {
+            Parser.Default.ParseArguments<CopyCommitsOptions, Options>(args)
+                .WithParsed<Options>(options => Options = options)
+                .WithParsed<CopyCommitsOptions>(options => CopyCommitsOptions = options);
+        }
+    }
+}
---
+++ GitTools/CLI/Options.cs
@@ -0,0 +1,34 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System.Collections.Generic;
+
+namespace Iswenzz.GitTools.CLI
+{
+    [Verb("copycommits", HelpText = "Copy commits from one repository to another")]
+    public class CopyCommitsOptions
+    {
+        [Option('m', "method", Required = true, HelpText = "The repository methods i.e: \"gitlab:git\"")]
+        public string Method { get; set; }
+
+        [Option('i', "input-url", Required = true, HelpText = "The input repository URL")]
+        public string InputURL { get; set; }
+
+        [Option('o', "output-url", Required = true, HelpText = "The output repository URL")]
+        public string OutputURL { get; set; }
+
+        [Usage(ApplicationAlias = "gittools")]
+        public static IEnumerable<Example> Examples
+        {
+            get => new List<Example> {
+                new Example("Copy the commits on a specific date, from a gitlab repository to a git repository", 
+                    new CopyCommitsOptions { 
+                        Method = "gitlab:git", 
+                        InputURL = "https://gitlab.com/gitlab-org/gitlab", 
+                        OutputURL = @"C:\Repository" 
+                    })
+            };
+        }
+    }
+
+    public class Options { }
+}
