2f93cc414544b3d4db6d191c03bda97bef8e2db5
Iswenzz <suxlolz1528@gmail.com>
2021-06-09 23:10:44 +0200

    EditorSelectableList: Opens a temp file in default editor and diff the updated lines

---
+++ GitTools/Utils/EditorSelectableList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Iswenzz.GitTools.Utils
+{
+    public class EditorSelectableList : IDisposable
+    {
+        public string FilePath { get; set; }
+        public Process Process { get; set; }
+
+        public EditorSelectableList()
+        {
+            FilePath = $"{Path.GetTempFileName()}.txt";
+        }
+
+        public IEnumerable<string> GetFileContent()
+        {
+            Process.WaitForExit();
+            return File.ReadAllLines(FilePath);
+        }
+
+        public void OpenWithContent(IEnumerable<string> lines)
+        {
+            File.WriteAllLines(FilePath, lines);
+            Process = new Process
+            {
+                StartInfo = new ProcessStartInfo(FilePath)
+                {
+                    UseShellExecute = true
+                }
+            };
+            Process.Start();
+        }
+
+        public void Dispose()
+        {
+            Process?.Dispose();
+        }
+    }
+}
