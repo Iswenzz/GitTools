3dcf34f825be83b153c7bada7ee581c29b77a0b5
Iswenzz <suxlolz1528@gmail.com>
2021-11-01 18:49:10 +0100

    Git: GetUserCommits with squashed commits

--- GitTools/Sys/Git.cs
+++ GitTools/Sys/Git.cs
@@ -1,6 +1,9 @@
-﻿using Iswenzz.GitTools.Data;
-using LibGit2Sharp;
+﻿using LibGit2Sharp;
 using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Iswenzz.GitTools.Data;
 
 namespace Iswenzz.GitTools.Sys
 {
@@ -17,7 +20,31 @@ namespace Iswenzz.GitTools.Sys
         /// <param name="repoPath">Git repository path.</param>
         public Git(string repoPath)
         {
-            Repository = new Repository(repoPath);
+            Repository = new(repoPath);
+        }
+
+        /// <summary>
+        /// Get the user's commits (working with squashed commits).
+        /// </summary>
+        public List<GitCommit> GetUserCommits(User user,
+            DateTime sinceDate = default, DateTime untilDate = default)
+        {
+            DateTime since = sinceDate;
+            DateTime until = untilDate != DateTime.MinValue ? untilDate : DateTime.Now;
+
+            List<GitCommit> userCommits = new();
+            foreach (GitObject o in Repository.ObjectDatabase)
+            {
+                try
+                {
+                    Commit c = o.Peel<Commit>();
+                    if (c.Committer.Email == user.EMail &&
+                        c.Committer.When >= since && c.Committer.When <= until)
+                        userCommits.Add(new GitCommit(c));
+                }
+                catch { }
+            }
+            return userCommits;
         }
 
         /// <summary>
@@ -26,7 +53,7 @@ namespace Iswenzz.GitTools.Sys
         /// <param name="commit">The commit informations.</param>
         public void Commit(GitCommit commit)
         {
-            CommitOptions options = new CommitOptions
+            CommitOptions options = new()
             {
                 AllowEmptyCommit = true
             };
@@ -36,20 +63,18 @@ namespace Iswenzz.GitTools.Sys
         /// <summary>
         /// Commit to the git repository with the specified GitCommit object.
         /// </summary>
+        /// <param name="user">The user to commit.</param>
         /// <param name="message">The commit message.</param>
         /// <param name="date">The commit date.</param>
-        public void Commit(string message, DateTime date = default)
+        public void Commit(User user, string message, DateTime date = default)
         {
             if (date == DateTime.MinValue)
                 date = DateTime.Now;
 
-            Signature author = new Signature(
-                Program.Settings.User.Username, 
-                Program.Settings.User.EMail,
-                date);
+            Signature author = new(user.Username, user.EMail, date);
             Signature committer = author;
 
-            CommitOptions options = new CommitOptions
+            CommitOptions options = new()
             {
                 AllowEmptyCommit = true
             };
