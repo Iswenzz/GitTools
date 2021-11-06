8e12fec054130b67b8386ad1317024a70ab27194
Iswenzz <suxlolz1528@gmail.com>
2021-11-06 17:02:31 +0100

    Git: Add/Remove remote & CherryPick

--- GitTools/Sys/Git.cs
+++ GitTools/Sys/Git.cs
@@ -1,9 +1,6 @@
-﻿using LibGit2Sharp;
-using System;
-using System.Linq;
+﻿using System;
 using System.Collections.Generic;
-
-using Iswenzz.GitTools.Data;
+using LibGit2Sharp;
 
 namespace Iswenzz.GitTools.Sys
 {
@@ -24,49 +21,89 @@ namespace Iswenzz.GitTools.Sys
         }
 
         /// <summary>
-        /// Get the user's commits (working with squashed commits).
+        /// Add a remote with a specific name and url, then update the repository remotes.
         /// </summary>
-        public List<GitCommit> GetUserCommits(User user,
-            DateTime sinceDate = default, DateTime untilDate = default)
+        /// <param name="name">The remote name.</param>
+        /// <param name="url">The remote URL.</param>
+        public void AddRemote(string name, string url)
+        {
+            if (Repository.Network.Remotes[name] == null)
+            {
+                Repository.Network.Remotes.Add(name, url);
+                Repository.Network.Fetch(name, Array.Empty<string>());
+            }
+        }
+
+        /// <summary>
+        /// Remove a specific remote.
+        /// </summary>
+        /// <param name="name">The remote name.</param>
+        public void RemoveRemote(string name) =>
+            Repository.Network.Remotes.Remove(name);
+
+        /// <summary>
+        /// Predicate to check if an object exists in this repository.
+        /// </summary>
+        /// <typeparam name="T">Class extending <see cref="GitObject"/>.</typeparam>
+        /// <param name="obj">The <see cref="GitObject"/>.</param>
+        /// <returns></returns>
+        public bool ObjectExists<T>(T obj) where T : GitObject =>
+            Repository.Lookup(obj.Id) != null;
+
+        /// <summary>
+        /// Get the commits in a specific time range and can be filtered by email. 
+        /// Working with squashed commits as well.
+        /// </summary>
+        public List<Commit> GetCommits(DateTime sinceDate = default, DateTime untilDate = default, 
+            string email = null)
         {
             DateTime since = sinceDate;
             DateTime until = untilDate != DateTime.MinValue ? untilDate : DateTime.Now;
 
-            List<GitCommit> userCommits = new();
+            List<Commit> commits = new();
             foreach (GitObject o in Repository.ObjectDatabase)
             {
                 try
                 {
                     Commit c = o.Peel<Commit>();
-                    if (c.Committer.Email == user.EMail &&
-                        c.Committer.When >= since && c.Committer.When <= until)
-                        userCommits.Add(new GitCommit(c));
+                    if (!string.IsNullOrEmpty(email) && c.Committer.Email != email)
+                        continue;
+                    if (c.Committer.When < since || c.Committer.When > until)
+                        continue;
+                    commits.Add(c);
                 }
-                catch { }
+                catch (InvalidSpecificationException) { }
             }
-            return userCommits;
+            return commits;
         }
 
         /// <summary>
-        /// Commit to the git repository with the specified GitCommit object.
+        /// Cherry-pick a specific commit.
         /// </summary>
-        /// <param name="commit">The commit informations.</param>
-        public void Commit(GitCommit commit)
+        /// <param name="commit">The commit to cherry-pick.</param>
+        public void CherryPick(Commit commit)
         {
-            CommitOptions options = new()
+            try
             {
-                AllowEmptyCommit = true
-            };
-            Repository.Commit(commit.Message, commit.Author, commit.Committer, options);
+                CherryPickOptions cpOptions = new() { FileConflictStrategy = CheckoutFileConflictStrategy.Theirs };
+                CherryPickResult cherryPickResult = Repository.CherryPick(commit, commit.Committer, cpOptions);
+
+                if (cherryPickResult.Status == CherryPickStatus.Conflicts)
+                {
+                    Commands.Stage(Repository, "*");
+                    Repository.Commit(commit.Message, commit.Author, commit.Committer);
+                }
+            }
+            catch { }
         }
 
         /// <summary>
-        /// Commit to the git repository with the specified GitCommit object.
+        /// Create an empty commit to the git repository.
         /// </summary>
         /// <param name="user">The user to commit.</param>
         /// <param name="message">The commit message.</param>
         /// <param name="date">The commit date.</param>
-        public void Commit(User user, string message, DateTime date = default)
+        public void MockCommit(User user, string message, DateTime date = default)
         {
             if (date == DateTime.MinValue)
                 date = DateTime.Now;
